96fa636e38fbf84dd8ddf5e3bca616ae09ec53dd
briancsp <runnerb2003@gmail.com>
2025-04-17 03:26:04 -0700

    fix var in other test

--- test_cnn.cu
+++ test_cnn.cu
@@ -110,8 +110,8 @@ void test_layer_forward() {
 
     ConvolutionalLayer* layer = createConvolutionalLayer(1, 1, 1, 2, 2, input);
     float filterData[] = {1, 0, 0, 1};
-    setDeviceMatrixData((input->filters)[0][0], filterData, FILTER_SIZE);
-    /*
+    setDeviceMatrixData((layer->filters)[0][0], filterData, FILTER_SIZE);
+    
     layerForward(layer, 0);
     
     float res[OUTPUT_SIZE];
@@ -129,7 +129,7 @@ void test_layer_forward() {
       printf("FAILED\n");
       exit(EXIT_FAILURE);
     }
-*/
+
     printf("\nPASSED\n\n");
 }
 
