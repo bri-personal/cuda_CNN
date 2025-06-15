12f9832fa6880fd3038bcaef97bf96762e0a9b94
briancsp <runnerb2003@gmail.com>
2025-06-14 17:12:36 -0700

    add test

--- cnn2/main_gpu.cu
+++ cnn2/main_gpu.cu
@@ -3,19 +3,6 @@
 #include "matrix.h"
 #include "cuda_matrix.cuh"
 
-#define BATCH_SIZE 2
-#define IN_CHANNELS 2
-#define IMG_HEIGHT 3
-#define IMG_WIDTH 3
-
-#define OUT_CHANNELS 2
-#define FILTER_HEIGHT 2
-#define FILTER_WIDTH 2
-
-#define PADDING 0
-#define STRIDE 1
-
-
 int gemmTest() {
   const int I = 8;
   const int K = 6;
@@ -38,7 +25,7 @@ int gemmTest() {
   deviceMatrixMult(deviceA, deviceB, deviceC, I*J);
   getDeviceMatrixData(hostC2.data, deviceC, I*J);
 
-  if(matrixEquals(&hostC2, &hostC, 0.000001)) {
+  if(!matrixEquals(&hostC2, &hostC, 0.000001)) {
     printf("CPU and GPU GEMM are NOT equal\n");
     freeMatrix(deviceA);
     freeMatrix(deviceB);
@@ -61,6 +48,63 @@ int im2colFlattenTest() {
   return 0;
 }
 
+int cpuConvTest() {
+  #define BATCH_SIZE 2
+  #define IN_CHANNELS 2
+  #define IMG_HEIGHT 3
+  #define IMG_WIDTH 3
+
+  #define OUT_CHANNELS 2
+  #define FILTER_HEIGHT 2
+  #define FILTER_WIDTH 2
+
+  #define PADDING 0
+  #define STRIDE 1
+
+  elem_t iData[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, -1, -2, -3, -4, -5, -6, -7, -8, -9, 11, 12, 13, 14, 15, 16, 17, 18, 19, -11, -12, -13, -14, -15, -16, -17, -18, -19};
+  Tensor4D i = {BATCH_SIZE, IN_CHANNELS, IMG_HEIGHT, IMG_WIDTH, iData};
+
+  elem_t kData[] = {0, 1, 1, 0, -1, 0, 0, -1, 2, 0, 0, 2, 0, -2, -2, 0};
+  Tensor4D k = {OUT_CHANNELS, IN_CHANNELS, FILTER_HEIGHT, FILTER_WIDTH, kData};
+
+  int outputWidth = OUTPUT_DIM(IMG_WIDTH, FILTER_WIDTH, PADDING, STRIDE);
+  int outputHeight = OUTPUT_DIM(IMG_HEIGHT, FILTER_HEIGHT, PADDING, STRIDE);
+  int outputArea = outputWidth*outputHeight;
+  int kernelArea = k.height*k.width;
+  int imageUnfoldedHeight = outputArea*i.dim4;
+  int imageUnfoldedWidth = i.depth*kernelArea;
+
+  Matrix iUnfolded = {imageUnfoldedHeight, imageUnfoldedWidth, (elem_t*) calloc(imageUnfoldedHeight*imageUnfoldedWidth, sizeof(elem_t))};
+  Matrix kFlattened = {imageUnfoldedWidth, k.dim4, (elem_t*) calloc(imageUnfoldedWidth*k.dim4, sizeof(elem_t))};
+
+  im2colUnfold4D_CPU(&iUnfolded, &i, k.width, kernelArea, outputWidth, outputArea);
+  im2colFlatten4D_CPU(&kFlattened, &k);
+
+  Matrix im2colConvOutput = {iUnfolded.height, kFlattened.width,
+    (elem_t*) calloc(imageUnfoldedHeight*imageUnfoldedWidth, sizeof(elem_t))};
+  gemm_CPU(&im2colConvOutput, &iUnfolded, &kFlattened);
+
+  Tensor4D convOutput = {BATCH_SIZE, OUT_CHANNELS, outputHeight, outputWidth,
+    (elem_t*) calloc(BATCH_SIZE*OUT_CHANNELS*outputArea, sizeof(elem_t))};
+  conv_CPU(&convOutput, &i, &k);
+
+  if(!im2colMatrixEqualsConvTensor4D(&im2colConvOutput, &convOutput, 0.000001)) {
+    printf("CPU im2col GEMM and CPU conv are NOT equal\n");
+    free(iUnfolded.data);
+    free(kFlattened.data);
+    free(im2colConvOutput.data);
+    free(convOutput.data);
+    return 1;
+  }
+
+  printf("CPU im2col GEMM and CPU conv are equal\n");
+  free(iUnfolded.data);
+  free(kFlattened.data);
+  free(im2colConvOutput.data);
+  free(convOutput.data);
+  return 0;
+}
+
 int convTest() {
   return 0;
 }
@@ -71,61 +115,8 @@ int main() {
   test_total += gemmTest();
   test_total += im2colUnfoldTest();
   test_total += im2colFlattenTest();
+  test_total += cpuConvTest();
   test_total += convTest();
 
   return test_total;
-
-
-
-  // elem_t iData[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, -1, -2, -3, -4, -5, -6, -7, -8, -9, 11, 12, 13, 14, 15, 16, 17, 18, 19, -11, -12, -13, -14, -15, -16, -17, -18, -19};
-  // Tensor4D i = {BATCH_SIZE, IN_CHANNELS, IMG_HEIGHT, IMG_WIDTH, iData};
-  // printImage4D(&i);
-
-  // elem_t kData[] = {0, 1, 1, 0, -1, 0, 0, -1, 2, 0, 0, 2, 0, -2, -2, 0};
-  // Tensor4D k = {OUT_CHANNELS, IN_CHANNELS, FILTER_HEIGHT, FILTER_WIDTH, kData};
-  // printFilter4D(&k);
-
-  // int outputWidth = OUTPUT_DIM(IMG_WIDTH, FILTER_WIDTH, PADDING, STRIDE);
-  // int outputHeight = OUTPUT_DIM(IMG_HEIGHT, FILTER_HEIGHT, PADDING, STRIDE);
-  // int outputArea = outputWidth*outputHeight;
-  // int kernelArea = k.height*k.width;
-  // int imageUnfoldedHeight = outputArea*i.dim4;
-  // int imageUnfoldedWidth = i.depth*kernelArea;
-
-  // Matrix iUnfolded = {imageUnfoldedHeight, imageUnfoldedWidth, (elem_t*) calloc(imageUnfoldedHeight*imageUnfoldedWidth, sizeof(elem_t))};
-  // Matrix kFlattened = {imageUnfoldedWidth, k.dim4, (elem_t*) calloc(imageUnfoldedWidth*k.dim4, sizeof(elem_t))};
-
-  // im2colUnfold4D_CPU(&iUnfolded, &i, k.width, kernelArea, outputWidth, outputArea);
-  // im2colFlatten4D_CPU(&kFlattened, &k);
-
-  // printf("I unfolded\n");
-  // printMatrix(&iUnfolded);
-
-  // printf("K flattened\n");
-  // printMatrix(&kFlattened);
-
-  // Matrix im2colConvOutput = {iUnfolded.height, kFlattened.width,
-  //   (elem_t*) calloc(imageUnfoldedHeight*imageUnfoldedWidth, sizeof(elem_t))};
-  // gemm_CPU(&im2colConvOutput, &iUnfolded, &kFlattened);
-
-  // printf("Im2Col Conv Output\n");
-  // printMatrix(&im2colConvOutput);
-
-  // Tensor4D convOutput = {BATCH_SIZE, OUT_CHANNELS, outputHeight, outputWidth,
-  //   (elem_t*) calloc(BATCH_SIZE*OUT_CHANNELS*outputArea, sizeof(elem_t))};
-
-  // conv_CPU(&convOutput, &i, &k);
-
-  // printf("Naive Conv Output\n");
-  // printImage4D(&convOutput);
-
-  // printf("im2colConvOutput == itself: %d\n", matrixEquals(&im2colConvOutput, &im2colConvOutput, 0.000001));
-  // printf("im2colConvOutput == convOutput: %d\n", im2colMatrixEqualsConvTensor4D(&im2colConvOutput, &convOutput, 0.000001));
-
-  // free(iUnfolded.data);
-  // free(kFlattened.data);
-  // free(im2colConvOutput.data);
-  // free(convOutput.data);
-
-  return 0;
 }
