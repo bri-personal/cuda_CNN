b06820c74808c4d52683d745c4f9405fd77d9177
briancsp <runnerb2003@gmail.com>
2025-06-14 21:55:58 -0700

    debug 17

--- cnn2/main_gpu.cu
+++ cnn2/main_gpu.cu
@@ -318,17 +318,13 @@ int convTest() {
   }
 
   printf("SUCCESS: CPU and GPU conv are equal\n");
-  free(hostInput.data);
-  //freeTensor4D(deviceInput);
-  free(hostKernel.data);
-  
-  //freeTensor4D(deviceKernel);
-
-
-
-  free(hostResultTensor4D.data);
-  free(hostResultMatrix.data);
-  //freeMatrix(deviceResult);
+  // free(hostInput.data);
+  // freeTensor4D(deviceInput);
+  // free(hostKernel.data);
+  // freeTensor4D(deviceKernel);
+  // free(hostResultTensor4D.data);
+  // free(hostResultMatrix.data);
+  // freeMatrix(deviceResult);
   return 0;
 }
 
