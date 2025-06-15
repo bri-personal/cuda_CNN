6cedd8deddb9687e2f2bd497d43252151fe4a914
briancsp <runnerb2003@gmail.com>
2025-06-14 19:53:30 -0700

    figure out which test is crashing 1

--- cnn2/main_gpu.cu
+++ cnn2/main_gpu.cu
@@ -323,10 +323,10 @@ int main() {
   int test_total = 0;
 
   test_total += gemmTest();
-  test_total += im2colUnfoldTest();
-  test_total += im2colFlattenTest();
+  // test_total += im2colUnfoldTest();
+  // test_total += im2colFlattenTest();
   // test_total += cpuConvTest();
-  test_total += convTest();
+  // test_total += convTest();
 
   return test_total;
 }
