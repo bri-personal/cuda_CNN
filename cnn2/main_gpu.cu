fc4187636e4980e7037f4df12b6341e40c0d4cf8
briancsp <runnerb2003@gmail.com>
2025-06-14 22:47:02 -0700

    run all tests

--- cnn2/main_gpu.cu
+++ cnn2/main_gpu.cu
@@ -332,11 +332,11 @@ int convTest() {
 int main() {
   int test_total = 0;
 
-  // test_total += gemmTest();
-  // test_total += im2colUnfoldTest();
-  // test_total += im2colFlattenTest();
-  // test_total += cpuConvTest();
+  test_total += gemmTest();
+  test_total += im2colUnfoldTest();
+  test_total += im2colFlattenTest();
   test_total += convTest();
+  // test_total += cpuConvTest();
 
   return test_total;
 }
