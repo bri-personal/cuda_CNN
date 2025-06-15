5b83eea8e3c29e7eda4d10268a6c9a602ebdcdc7
briancsp <runnerb2003@gmail.com>
2025-06-15 00:06:34 -0700

    fix

--- cnn2/cnn.cu
+++ cnn2/cnn.cu
@@ -9,15 +9,15 @@
 
 void initConvolutionalModel(ConvolutionalModel** model, int batchSize, float learningRate) {  
     *model = (ConvolutionalModel*) calloc(1, sizeof(ConvolutionalModel));
-    if (!model) { perror("calloc model"); exit(1); }
+    if (!(*model)) { perror("calloc model"); exit(1); }
 
     ConvolutionalNetwork *cnn = (ConvolutionalNetwork*) malloc(sizeof(ConvolutionalNetwork));
     if (!cnn) { perror("malloc network"); exit(1); }
     cnn->numLayers = 0;
   
-    model->network = cnn;
-    model->learningRate = learningRate;
-    model->batchSize = batchSize;
+    *model->network = cnn;
+    *model->learningRate = learningRate;
+    *model->batchSize = batchSize;
   
     checkError("Init CNN");
   }
