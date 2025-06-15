8df88b0f129c7287fe2ed3b4050e9063551571a8
briancsp <runnerb2003@gmail.com>
2025-06-15 00:27:23 -0700

    a

--- cnn2/gpu_cnn_test.cu
+++ cnn2/gpu_cnn_test.cu
@@ -9,13 +9,32 @@ int initModelTest() {
     int batchSize = 10;
     float learningRate = 0.025;
 
+    int inChannels = 3;
+    int inRows = 7;
+    int inCols = 9;
+
+    int hiddenChannels = 5;
+    int hiddenRows = 5;
+    int hiddenCols = 7;
+
+    int outChannels = 2;
+    int outRows = 3;
+    int outCols = 5;
+
     ConvolutionalModel* model;
     initConvolutionalModel(&model, batchSize, learningRate);
+    addInputLayer(model, inChannels, inRows, inCols);
+    addConvLayer(model, hiddenChannels, hiddenRows, hiddenCols);
+    addConvLayer(model, outChannels, outRows, outCols);
 
-    if(model->batchSize != 10 || fabsf(model->learningRate - 0.025) > 0.000001) {
+    if(model->batchSize != 10 || fabsf(model->learningRate - 0.025) > 0.000001 ||
+        model->inChannels != inChannels || model->inHeight != inRows || model->inWidth != inChannels ||
+        model->outChannels != outChannels || model->outHeight != outRows || model->outWidth != outCols) {
         printf("FAILURE: model params NOT correct\n");
         return 1;
     }
+
+    ConvolutionalNetwork* net = model->network;
 
     printf("SUCCESS: model params correct\n");
     return 0;
